5aed7d21e32e3359e4d0f9a773dd90be9cbe98f6
Ohad Elkayam <ohadelkayam@mail.tau.ac.il>
2023-09-30 12:52:18 +0300

    c and h

--- symnmf.h
+++ symnmf.h
@@ -6,9 +6,17 @@
 #define MAX_ITER 300
 #define EPS 0.0001
 
-typedef struct PointType Point;
-typedef struct ListType PointList;
+typedef struct PointType {
+    double *data;
+    int length;
+} Point;
+
+typedef struct ListType {
+    Point *pointsArr;
+    int length;
+} PointList;
 
 double** sym(PointList *pointList);
 double** ddg(PointList *pointList);
 double** norm(PointList *pointList);
+double** symnmf(double** h0, double** w, int rows, int columns);
